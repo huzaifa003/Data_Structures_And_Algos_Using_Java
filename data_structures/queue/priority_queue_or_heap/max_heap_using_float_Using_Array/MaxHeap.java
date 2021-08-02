90d46d577ccb908634bb1ada2892b73cc99dd9a4
huzaifa003 <buntyhuzaifa3@gmail.com>
2021-08-03 02:04:30 +0500

    Added comments to each method

--- data_structures/queue/priority_queue_or_heap/max_heap_using_float_Using_Array/MaxHeap.java
+++ data_structures/queue/priority_queue_or_heap/max_heap_using_float_Using_Array/MaxHeap.java
@@ -4,6 +4,7 @@ package priority_queue_or_heap.max_heap_using_float_Using_Array;
     * it's normally implemented by the arrays
     * in Max Heap the child is smaller than its parent
     * formula for parent at a specific position is position/2th index
+    * the 0th index is always kept empty in an array!
  */
 public class MaxHeap {
     float heap[];
@@ -14,24 +15,24 @@ public class MaxHeap {
         length = 0;
     }
 
-    public void maxHeapify(int position) {
+    public void maxHeapify(int position) { //if child is greater than we swap the parent and child!
         while (position > 1 && heap[position / 2] < heap[position]) {
             float temp = heap[position];
             heap[position] = heap[position / 2];
             heap[position / 2] = temp;
-            position = position / 2;
+            position = position / 2; //reaching parent of every child
         }
     }
 
     public void insert(float data) {
         this.length++;
         heap[this.length] = data;
-        maxHeapify(this.length);
+        maxHeapify(this.length); //swaping the data from LAST index aka length until child is smaller than parent
     }
 
     public void display() {
         for (int i = 1; i <= length + 1; i++) {
-            System.out.println(heap[i]);
+            System.out.println(heap[i]); //printing from index 1 as 0 index is always empty
         }
 
     }
