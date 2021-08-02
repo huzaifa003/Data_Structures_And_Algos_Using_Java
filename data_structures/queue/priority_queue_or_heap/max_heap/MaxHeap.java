09aed5a7f0db5f87f5346f00e8bdeb61c5940c77
huzaifa003 <buntyhuzaifa3@gmail.com>
2021-08-03 01:25:11 +0500

    Added Insert Heapify and Display methods to MaxHeap.java

--- data_structures/queue/priority_queue_or_heap/max_heap/MaxHeap.java
+++ data_structures/queue/priority_queue_or_heap/max_heap/MaxHeap.java
@@ -8,4 +8,31 @@ public class MaxHeap {
         this.heap = new Object[size + 1];
         length = 0;
     }
+
+    public void maxHeapify(int position)
+    {
+        while (position > 1 &&  heap[position/2] .equals(heap[position]) )
+        {
+            Object temp =  heap[position];
+            heap[position] = heap[position/2];
+            heap[position/2] = temp;
+            position  = position/2;
+        }
+    }
+
+    public void insert(Object data)
+    {
+        length++;
+        heap[length] = data;
+        maxHeapify(length);
+    }
+
+    public void display()
+    {
+        for (Object data :
+                heap) {
+            System.out.println(data);
+        }
+        System.out.println();
+    }
 }
