166b8fb83eb6efef2d22a6d7275b6fd47c1e792b
huzaifa003 <buntyhuzaifa3@gmail.com>
2021-08-27 12:56:23 +0500

    added display methods to Trie Completed all the trials to tests

--- data_structures/trie/Runner.java
+++ data_structures/trie/Runner.java
@@ -1,10 +1,48 @@
+import java.util.ArrayList;
+
 public class Runner {
     public static void main(String[] args) {
+        //----------------------- Trial of Insertion in Trie-----------------------\\
         Trie trie = new Trie();
         trie.insert("cat");
         trie.insert("cab");
         trie.insert("ca");
         trie.insert("cassy");
+
+        //--------------------- Trial of contains() in Trie------------------------\\
+
         System.out.println(trie.contains("cass"));
+
+        //-------------------- Trial of displayAllWords() in Trie aka using root -----------------\\
+
+        ArrayList<String> words = trie.displayAllWords();
+        for (String word :
+                words) {
+            System.out.println(word);
+        }
+
+        //------------------- Trial of searchNodeUsingPrefix() -------------------\\
+
+        TrieNode temp = trie.searchNodeUsingPrefix("ca");
+        System.out.println(temp.children[1]); //since this index 1 which represents b is not empty it means b is present as in "cab"
+
+        //------------------- Trial of displayAllWords(String prefix) using searchNodeUsingPrefix() for autocomplete -------\\
+
+        trie.insert("bossy");
+        trie.insert("boss");
+
+        words = trie.displayAllWordsUsingPrefix("ca");
+        for (String word :
+                words) {
+            System.out.println(word);
+        }
+
+        words = trie.displayAllWordsUsingPrefix("boss");
+        for (String word :
+                words) {
+            System.out.println(word);
+        }
+
+
     }
 }
--- data_structures/trie/Trie.java
+++ data_structures/trie/Trie.java
@@ -1,40 +1,42 @@
+import java.util.ArrayList;
+
 /*
-    * Trie data structure is used to give search results
-    * ROOT is EMPTY when initialized
-    * LATER it points to all characters as starting character
-    * it has allot of nodes (arrays) pointing to one another to represent a word
-    * TO GET INDEX OF char we can subtract it from the ascii value of 'a'
-    * * for example: if we subtract 'a' from 'a' we get 0, which is gonna be the index value of trieNode Array
-    *
+ * Trie data structure is used to give search results
+ * ROOT is EMPTY when initialized
+ * LATER it points to all characters as starting character
+ * it has allot of nodes (arrays) pointing to one another to represent a word
+ * TO GET INDEX OF char we can subtract it from the ascii value of 'a'
+ * * for example: if we subtract 'a' from 'a' we get 0, which is gonna be the index value of trieNode Array
+ *
  */
 public class Trie {
     TrieNode root;
+
     public Trie() {
         root = new TrieNode(); //ROOT IS EMPTY
     }
 
     /*
-        * Insertion means making allot of trieNods and pointing them to one another
-        * THERE ARE 3 cases of inserting any word
-        *
-        * CASE -1 :- Trie is empty
-        * -> we simply keep creating triedNodes and keep em pointing to one another until each character is entered
-        * -> When end is reached we make isWord as true
-        *
-        * Case -2 :- Non-Empty Trie but no prefix to the previous word
-        * -> we create another branch and repeat the case#1
-        *
-        * Case-3 :- Non-Empty Trie but WITH prefix matching the previous word
-        * -> we simply use the all the common letters of common word and when there are no longer common words (in sequence) we create another branch to add remaining words
-        *
-        * Case-4 :- Non-Empty Trie WITH word already present
-        * -> Now in this case if word is already present such as if we want to insert "dad" but trie already has word "daddy"
-        * -> we mark the d (where dad ends) as isWord as well
+     * Insertion means making allot of trieNods and pointing them to one another
+     * THERE ARE 3 cases of inserting any word
+     *
+     * CASE -1 :- Trie is empty
+     * -> we simply keep creating triedNodes and keep em pointing to one another until each character is entered
+     * -> When end is reached we make isWord as true
+     *
+     * Case -2 :- Non-Empty Trie but no prefix to the previous word
+     * -> we create another branch and repeat the case#1
+     *
+     * Case-3 :- Non-Empty Trie but WITH prefix matching the previous word
+     * -> we simply use the all the common letters of common word and when there are no longer common words (in sequence) we create another branch to add remaining words
+     *
+     * Case-4 :- Non-Empty Trie WITH word already present
+     * -> Now in this case if word is already present such as if we want to insert "dad" but trie already has word "daddy"
+     * -> we mark the d (where dad ends) as isWord as well
      *
      */
 
-    public void insert(String word)
-    {
+    public void insert(String word) {
         word = word.toLowerCase(); //converting to lower case cause we have only made node of 26 characters (for only 1 case)
 
         TrieNode current = root; //whenever we initialize the root its empty;
@@ -47,9 +49,7 @@ public class Trie {
             {
                 current.children[index] = new TrieNode();  //then we create another TrieNode at the index indicating that there is a letter present
                 current = current.children[index]; // moving current ahead of root to insert the  next letter
-            }
-            else
-            {
+            } else {
                 current = current.children[index]; //if node is already present (means letter is already there) we simply jump
             }
         }
@@ -57,15 +57,13 @@ public class Trie {
         current.isWord = true; //representing end of the word
     }
 
-    private int getIndex(char character)
-    {
-            // TO GET INDEX OF char we can subtract it from the ascii value of 'a'
-            // for example: if we subtract 'a' from 'a' we get 0, which is gonna be the index value of trieNode Array
+    private int getIndex(char character) {
+        // TO GET INDEX OF char we can subtract it from the ascii value of 'a'
+        // for example: if we subtract 'a' from 'a' we get 0, which is gonna be the index value of trieNode Array
         return character - 'a';
     }
 
-    public boolean contains(String word)
-    {
+    public boolean contains(String word) {
         boolean flag = true;
 
         word = word.toLowerCase();
@@ -74,13 +72,11 @@ public class Trie {
             char character = word.charAt(i); //getting char from string
             int index = getIndex(character); //getting index
 
-            if (current.children[index] == null ) //we used index cause index represents the character for example 0 will represent 'a' if a trieNode exists on that index it indicates the presence of that char
+            if (current.children[index] == null) //we used index cause index represents the character for example 0 will represent 'a' if a trieNode exists on that index it indicates the presence of that char
             {
                 flag = false; //if any character fails to match or doesn't exist flag is false
                 break;
-            }
-            else
-            {
+            } else {
                 current = current.children[index]; //moving to next character if it matchs
             }
         }
@@ -91,5 +87,72 @@ public class Trie {
         }
 
         return flag;
+    }
+
+
+    public TrieNode searchNodeUsingPrefix(String prefix) {
+        /*
+         * this method iterates till the required prefix is found
+         * this is used for autocompletion as in google search results
+         */
+        prefix = prefix.toLowerCase();
+        TrieNode current = root;
+        int i;
+        for (i = 0; i < prefix.length(); i++) {
+            int index = getIndex(prefix.charAt(i));
+            if (current.children[index] == null) {
+                break;
+            } else {
+                current = current.children[index];
+            }
+        }
+
+        if (i < prefix.length() - 1) {
+            return null;
+        } else {
+            return current;
+        }
+    }
+
+    public ArrayList<String> displayAllWords() {
+        /*
+            * here we use root cause we need to display all words, and prefix is empty string cause in root there is no prefix
+         */
+        TrieNode trieNode = root;
+        if (trieNode != null) {
+            ArrayList<String> words = new ArrayList<>();
+            addAllWords(trieNode, "", words);
+            return words;
+        }
+        return null;
+    }
+
+    public ArrayList<String> displayAllWordsUsingPrefix(String prefix) {
+        /*
+            * we first get the required prefix node using searchNodeUsingPrefix(prefix)
+            * then it passes the already found word as prefix such as "ca" etc
+            * then it returns words after passing it to addAllWords()
+         */
+        TrieNode trieNode = searchNodeUsingPrefix(prefix);
+        if (trieNode != null) {
+            ArrayList<String> words = new ArrayList<>();
+            addAllWords(trieNode, prefix, words);
+            return words;
+        } else {
+            return null;
+        }
+    }
+
+
+    private void addAllWords(TrieNode node, String word, ArrayList<String> words) {
+        if (node.isWord) { //if end of word is reached that word is appended in ArrayList of words
+            words.add(word);
+        }
+        for (int index = 0; index < 26; index++) //iterating through each index of each node
+        {
+            if (node.children[index] != null) { //if index is not null then
+                addAllWords(node.children[index], word + (char) (index + 'a'), words); //* going into that index using recursion by passing that node, and adding the previous character to word string and passing it
+            }
+        }
     }
 }
